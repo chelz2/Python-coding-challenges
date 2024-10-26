6d827bfbcb5c1fab15e9118a6ffe8dfd282175ff
chelz2 <golirida@hotmail.com>
2024-10-26 23:06:55 +0530

    arrray challenge - finding missing element

---
+++ FindMissingElement.py
@@ -0,0 +1,12 @@
+# Finding Missing element in an array 
+"""
+problem - consider an array of non negative integer, second array is created by shuffling the first array & then removing 
+a random element from it. i.e We have to find the missing number in second array. 
+
+eg- finder([1,2,3,4], [2,4,1]) --> output : 3 is missing
+"""
+
+def finder(arr1, arr2):
+    pass
+
+
