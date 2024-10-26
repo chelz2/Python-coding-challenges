102fe61a1002b1c1eac2e92941281d760efad554
chelz2 <golirida@hotmail.com>
2024-10-26 23:27:48 +0530

    arrray challenge - finding missing element

--- FindMissingElement.py
+++ FindMissingElement.py
@@ -7,6 +7,14 @@ eg- finder([1,2,3,4], [2,4,1]) --> output : 3 is missing
 """
 
 def finder(arr1, arr2):
-    pass
+    arr1.sort()
+    arr2.sort()
+
+    for num1, num2 in zip(arr1, arr2):
+        if num1 != num2:
+            return num1
+
+    return num1[-1]
 
 
+finder([1,2,4,3], [4,2,3])
